aa4deffe8c72b3fbf84d2d0adda9c97bcd19f656
Артем Храбан <a.chastener@gmail.com>
2019-10-27 23:43:05 +0300

    add function for rating monitoring and some exceptions

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def send_welcome(message):
 
 
 @bot.message_handler(commands=['auth'])
-def show_rating(message):
+def auth(message):
     bot.send_message(message.from_user.id, 'Введите свой номер телефона !не в меджународном формате!')
     bot.register_next_step_handler(message, get_telephone)
 
@@ -41,6 +41,7 @@ def generate_code(message):
         bot.register_next_step_handler(message, get_telephone)
         return
 
+
 def identities(message, telephone):
     try:
         int(message.text)
@@ -52,7 +53,7 @@ def identities(message, telephone):
                  headers={'Authorization': key})
     if response.status_code == 200:
         student_id = response.json()['personId']
-        bot.send_message(message.from_user.id, student_id)
+        get_rating(message, student_id)
     else:
         bot.send_message(message.from_user.id, 'Некорректно введен код')
         bot.send_message(message.from_user.id, response.text)
@@ -60,7 +61,47 @@ def identities(message, telephone):
         return
 
 
+def get_rating(message, student_id):
+    semester = get_current_semester(student_id)
+    message.text = int(semester)-1
+    get_rating_loop(message, student_id, semester)
+    
+
+
+def get_rating_loop(message, student_id, semester):
+    try:
+        semester_id = int(message.text)
+        max_semester = int(semester)
+    except ValueError:
+        bot.send_message(message.from_user.id, 'Номер семестра не может содержать букв, введите заново')
+        bot.register_next_step_handler(message, get_rating_loop, student_id, semester)
+        return
+    if (semester_id <= max_semester) and (semester_id > 0):
+        response = requests.get(f'http://apis.kpi.ua/api/marks?studentId={student_id}&semesterId={semester_id}',
+                     headers={'Authorization': key})
+        if response.status_code != 200:
+            bot.send_message(message.from_user.id, 'Походу код аутентификации сдох, перезалогинтесь /auth')
+            return
+        else:
+            full_response = ''
+            for disciplines in response.json():
+                full_response += f'{disciplines["disciplineName"]}, {disciplines["studyTypeName"]}: {disciplines["mark"]}\n'
+            print(full_response)
+            bot.send_message(message.from_user.id, full_response)
+            bot.send_message(message.from_user.id, f'Если хотите посмотреть предыдущие, введите номер семестра\nВы сейчас на {max_semester}')
+            bot.register_next_step_handler(message, get_rating_loop, student_id, max_semester)
+    else:
+        bot.send_message(message.from_user.id, 'Семестр не может быть больше текущего, либо меньше 0\nВведите еще раз')
+        bot.register_next_step_handler(message, get_rating_loop, student_id, max_semester)
+
+
+def get_current_semester(student_id):
+    response = requests.get(f'http://apis.kpi.ua/api/information/current-semester/?studentId={student_id}',
+                 headers={'Authorization': key})
+    return response.json()['currentSemester']
+
+
 bot.polling()
 if __name__ == '__main__':
-    app.run()
+   app.run()
 
