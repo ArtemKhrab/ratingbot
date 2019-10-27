5379101ef459d547c6f32ece45b15ab3127ce7c8
Артем Храбан <a.chastener@gmail.com>
2019-10-27 11:28:02 +0300

    fix some cycles

--- app.py
+++ app.py
@@ -25,29 +25,42 @@ def get_telephone(message):
     except ValueError:
         bot.send_message(message.from_user.id, 'Введите, пожалуйста, номер без символов')
         bot.register_next_step_handler(message, get_telephone)
+        return
     generate_code(message)
 
 
 def generate_code(message):
     telephone = message.text
     response = requests.post(f'http://apis.kpi.ua/api/identities/secret/?phone={telephone}',
-                  headers={'Authorization': key})
-    print(response)
+                 headers={'Authorization': key})
     if response.status_code == 200:
         bot.send_message(message.from_user.id, 'Хорошо, теперь введите код')
-        #bot.register_next_step_handler(message, identities, telephone)
+        bot.register_next_step_handler(message, identities, telephone)
     elif response.status_code == 404 or response.status_code == 400:
         bot.send_message(message.from_user.id, 'Неправильно введен номер, либо не сущевствует в базе.\nВведите еще раз')
         bot.register_next_step_handler(message, get_telephone)
+        return
 
 def identities(message, telephone):
-    print(message.text)
-    print(telephone)
+    try:
+        int(message.text)
+    except ValueError:
+        bot.send_message(message.from_user.id, 'Введите код')
+        bot.register_next_step_handler(message, identities, telephone)
+        return
     response = requests.get(f'http://apis.kpi.ua/api/identities/?phone={telephone}&secret={message.text}',
                  headers={'Authorization': key})
-    print(response)
+    if response.status_code == 200:
+        student_id = response.json()['personId']
+        bot.send_message(message.from_user.id, student_id)
+    else:
+        bot.send_message(message.from_user.id, 'Некорректно введен код')
+        bot.send_message(message.from_user.id, response.text)
+        bot.register_next_step_handler(message, identities, telephone)
+        return
 
 
 bot.polling()
 if __name__ == '__main__':
     app.run()
+
